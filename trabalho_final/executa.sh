382904f05e2fe6ad19ee9ee73f58b15b09660501
felipeaq <felipe@example.com>
2019-12-09 18:28:12 -0200

    dados_simulados

--- trabalho_final/executa.sh
+++ trabalho_final/executa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in instancias/k*.dat instancias/F25.dat
 do
-    for j in $(seq 1 10); do  python arc_heuristic.py $i ; done
+    for j in $(seq 1 30); do  python arc_heuristic.py $i ; done
 
 done
